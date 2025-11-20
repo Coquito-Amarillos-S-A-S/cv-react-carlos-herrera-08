9de551ccc51f58fe83f2b8233e27996a92c736f2
CarlosHerrera1210 <ch7690362@gmail.com>
2025-11-19 22:30:31 -0500

    creacion del archivo cvData,js

---
+++ src/data/cvData.js
@@ -0,0 +1,39 @@
+export const cvData = {
+  nombre: "Carlos Herrera",
+  profesion: "Desarrollador Frontend",
+  perfil:
+    "Soy un desarrollador entusiasta con experiencia en React, JavaScript y creación de interfaces modernas.",
+  educacion: [
+    {
+      institucion: "SENA",
+      titulo: "Tecnólogo en Análisis y Desarrollo de Software",
+      año: "2024",
+    },
+  ],
+  experiencia: [
+    {
+      empresa: "Empresa Tech",
+      cargo: "Frontend Developer",
+      año: "2023 - 2024",
+      funciones: [
+        "Desarrollo de interfaces en React",
+        "Consumo de APIs REST",
+        "Optimización de componentes",
+      ],
+    },
+  ],
+  tecnologias: ["HTML", "CSS", "JavaScript", "React", "Git", "Node.js"],
+  proyectos: [
+    {
+      nombre: "API de Películas",
+      descripcion: "Proyecto CRUD completo con Express y SQLite",
+      link: "https://github.com/turepo",
+    },
+  ],
+  habilidades: [
+    "Responsabilidad",
+    "Comunicación",
+    "Trabajo en equipo",
+    "Liderazgo",
+  ],
+};
