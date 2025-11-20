e842be33c5e8a17f6e1006debcfac93bfec18827
CarlosHerrera1210 <ch7690362@gmail.com>
2025-11-19 22:43:02 -0500

    creacion del archivo app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CabeceraCV from "./components/CabeceraCV";
+import Perfil from "./components/Perfil";
+import Educacion from "./components/Educacion";
+import Experiencia from "./components/Experiencia";
+import StackTecnologias from "./components/StackTecnologias";
+import Proyectos from "./components/Proyectos";
+import Habilidades from "./components/Habilidades";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { cvData } from "./data/cvData";
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{ padding: 20 }}>
+      <CabeceraCV nombre={cvData.nombre} profesion={cvData.profesion} />
 
-export default App
+      <Perfil texto={cvData.perfil} />
+
+      <Educacion items={cvData.educacion} />
+
+      <Experiencia trabajos={cvData.experiencia} />
+
+      <StackTecnologias tecnologias={cvData.tecnologias} />
+
+      <Proyectos proyectos={cvData.proyectos} />
+
+      <Habilidades habilidades={cvData.habilidades} />
+    </div>
+  );
+}
